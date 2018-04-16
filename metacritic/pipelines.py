4c0cce676d74b0c4e6f39d7e889b2247f2c2eea6
Derian Reuss <derianreuss@Derians-MacBook-Pro.local>
2018-04-16 10:13:48 -0400

    Adding logic to update existing Trello Cards with score data

--- metacritic/pipelines.py
+++ metacritic/pipelines.py
@@ -4,8 +4,101 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
+"""
+Item pipeline code for processing items retrieved from MetaCritic
+"""
+
+from __future__ import print_function
+import json
+import re
+import os
+import trello
+from scrapy.exceptions import DropItem
+
+
+def get_trello_properties():
+    """
+    Read trello connection properties from the config file or environment variables
+
+    :return: Dictionary with trello connection parameters
+    """
+    config = {}
+    # Read properties from trello_config.json if it is available
+    # TODO Change the file format to something other than json (that allows comments)
+    try:
+        with open('trello_config.json', 'r') as config_file:
+            config = json.load(config_file)
+    except IOError:
+        print("Failed to read config file trello_config.json.  Falling back to reading environment variables")
+
+    # If Environment variables are set, they override the trello_config.json
+    if os.environ.get('TRELLO_API_KEY') is not None:
+        config['trello_api_key'] = os.environ.get('TRELLO_API_KEY')
+    if os.environ.get('TRELLO_API_SECRET') is not None:
+        config['trello_api_secret'] = os.environ.get('TRELLO_API_SECRET')
+    if os.environ.get('TRELLO_BOARD_ID') is not None:
+        config['trello_board_id'] = os.environ.get('TRELLO_BOARD_ID')
+
+    return config
 
 
 class MetacriticPipeline(object):
+    """
+    Metacritic Pipeline class that scrapy invokes for all items found by the metacritic spider
+    """
+    def __init__(self):
+        """
+        Initialization needs to connect to trello, and pull a dictionary of all titles/cards.
+        This makes it easy for process_item to update card titles and create new cards
+        """
+        config = get_trello_properties()
+        # TODO Remove debug printing
+        for item in config:
+            print(item, config[item])
+
+        # Initialize trello client
+        print("Inside Init")
+        self._client = trello.TrelloClient(
+            api_key=config['trello_api_key'],
+            api_secret=config['trello_api_secret'],
+            # token='your-oauth-token',
+            # token_secret='your-oauth-token-secret'
+        )
+
+        # Ensure that authorization passed, and that we can retrieve the board
+        try:
+            game_board = self._client.get_board(config['trello_board_id'])
+        except trello.Unauthorized:
+            print("Trello authorization failed.  Check api key and api secret.")
+            raise
+        except trello.ResourceUnavailable:
+            print("Trello couldn't retrieve the board.  Board id needs to be set to an existing board:")
+            print(' ', 'Board ID'.ljust(24), 'Board Name')
+            for board in self._client.list_boards():
+                print(' ', board.id.rjust(24), board.name)
+            raise
+
+        # Retrieve all the cards, and create a dictionary with all of the game titles and cards
+        self._card_collection = {}
+        for card in game_board.all_cards():
+            game_title = card.name
+            re_var = re.search('(.*) \\[\\d+\\]', card.name)
+            if re_var is not None:
+                game_title = re_var.group(1)
+            self._card_collection[game_title] = card
+
     def process_item(self, item, spider):
-        return item
+        """
+        Method to handle items found by the spider
+        :param item:   item found by the spider
+        :param spider:
+        :return:       item if criteria is met (in this case >= Critic Score 80)
+        """
+        if item['title'] in self._card_collection:
+            self._card_collection[item['title']].set_name(item['title'] +
+                                                          ' [' + item['cscore'] + ']')
+        if int(item['cscore']) >= 80:
+            return item
+        raise DropItem("Critic score too low for %s" % item)
+
+        #TODO Create new cards for games not already added
--- metacritic/spiders/metacritic_new_spider.py
+++ metacritic/spiders/metacritic_new_spider.py
@@ -1,17 +1,33 @@
+"""
+Spider class for scraping metacritic pages (from XBox One games, sorted by Metacritic scores)
+"""
+
 from scrapy.spider import BaseSpider
-from scrapy.selector import HtmlXPathSelector
 from scrapy import Selector
 from metacritic.items import MetacriticItem
 
 
+# TODO Change this to automatically pull more pages, instead of just the first 3
+
 class MetacriticSpider(BaseSpider):
+    """
+    Spider class for walking through metacritic pages (as required by scrapy)
+    """
     name = "metacriticnew" # Name of the spider, to be used when crawling
     allowed_domains = ["metacritic.com"] # Where the spider is allowed to go
     start_urls = [
-        "http://www.metacritic.com/browse/games/score/metascore/all/xboxone/filtered?sort=desc&page=0"
+        "http://www.metacritic.com/browse/games/score/metascore/all/xboxone/filtered?sort=desc&page=0",
+        "http://www.metacritic.com/browse/games/score/metascore/all/xboxone/filtered?sort=desc&page=1",
+        "http://www.metacritic.com/browse/games/score/metascore/all/xboxone/filtered?sort=desc&page=2"
     ]
 
     def parse(self, response):
+        """
+        Function to scrape pulled documents and create Metacritic item records
+
+        :param response: html page to scrape
+        :return:         list of items found on page
+        """
         sel = Selector(response)
         sites = sel.css("div.product_row")
         items = []
@@ -19,27 +35,9 @@ class MetacriticSpider(BaseSpider):
             item = MetacriticItem()
             item['title'] = ''.join(site.css("div.product_title a::text").extract()).strip()
             item['link'] = ''.join(site.css("div.product_title a::attr(href)").extract())
-            #item['cscore'] = site.select('div[@class="basic_stat product_score brief_metascore"]/div[contains( @class , "metascore_w small game positive")] / text()').extract()
-            #item['uscore'] = site.select('div[@class="more_stats condensed_stats"]/ul/li/span[contains( @class , "data textscore textscore")] / text()').extract()
             item['cscore'] = ''.join(site.css("div.metascore_w::text").extract())
             item['uscore'] = ''.join(site.css("span.textscore_favorable::text").extract())
             item['date'] = ''.join(site.css("div.product_date::text").extract()).strip()
             items.append(item)
 
-        #for game in response.css("div.game"):
-        #    print game
-        #    print game.css("div.product_title")
-
-        #hxs = HtmlXPathSelector(response)  # The XPath selector
-        #sites = hxs.select('//li[contains(@class, "product game_product")]/div[@class="product_wrap"]')
-        #items = []
-        #for site in sites:
-        #    item = MetacriticItem()
-        #    item['title'] = site.select('div[@class="basic_stat product_title"]/a/text()').extract()
-        #    item['link'] = site.select('div[@class="basic_stat product_title"]/a/@href').extract()
-        #    #item['cscore'] = site.select('div[@class="basic_stat product_score brief_metascore"]/div/div/span[contains( @class , "data metascore score")] / text()').extract()
-        #    item['cscore'] = site.select('div[@class="basic_stat product_score brief_metascore"]/div[contains( @class , "metascore_w small game positive")] / text()').extract()
-        #    item['uscore'] = site.select('div[@class="more_stats condensed_stats"]/ul/li/span[contains( @class , "data textscore textscore")] / text()').extract()
-        #    item['date'] = site.select('div[@class="more_stats condensed_stats"]/ul/li/span[@ class ="data"] / text()').extract()
-        #    items.append(item)
         return items
