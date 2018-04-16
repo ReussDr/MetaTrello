24edc8cfa234ab4f38a93401a89ba669a63996ab
Derian Reuss <derianreuss@Derians-MacBook-Pro.local>
2018-04-16 17:59:41 -0400

    Moving retrieval of trello_properties to a helper class, instead of a function inside the pipeline

--- metacritic/pipelines.py
+++ metacritic/pipelines.py
@@ -14,32 +14,10 @@ import re
 import os
 import trello
 from scrapy.exceptions import DropItem
+from trello_helpers.utils import get_trello_properties
 
 
-def get_trello_properties():
-    """
-    Read trello connection properties from the config file or environment variables
 
-    :return: Dictionary with trello connection parameters
-    """
-    config = {}
-    # Read properties from trello_config.json if it is available
-    # TODO Change the file format to something other than json (that allows comments)
-    try:
-        with open('trello_config.json', 'r') as config_file:
-            config = json.load(config_file)
-    except IOError:
-        print("Failed to read config file trello_config.json.  Falling back to reading environment variables")
-
-    # If Environment variables are set, they override the trello_config.json
-    if os.environ.get('TRELLO_API_KEY') is not None:
-        config['trello_api_key'] = os.environ.get('TRELLO_API_KEY')
-    if os.environ.get('TRELLO_API_SECRET') is not None:
-        config['trello_api_secret'] = os.environ.get('TRELLO_API_SECRET')
-    if os.environ.get('TRELLO_BOARD_ID') is not None:
-        config['trello_board_id'] = os.environ.get('TRELLO_BOARD_ID')
-
-    return config
 
 
 class MetacriticPipeline(object):
